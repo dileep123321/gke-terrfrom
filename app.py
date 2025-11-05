c3be3eef650e024a312eba68ed6463ee7365a182
Venu5198 <venukrishnaya@gmail.com>
2025-11-05 05:57:47 +0000

    Final Commit

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-class H(BaseHTTPRequestHandler):
+
+
+class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200); self.end_headers()
+        self.send_response(200)
+        self.end_headers()
         self.wfile.write(b"Hello from backend v1!")
-HTTPServer(("", 8080), H).serve_forever()
+
+
+def run():
+    server_address = ("", 8080)
+    httpd = HTTPServer(server_address, Handler)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
+
